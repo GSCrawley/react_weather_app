79b944001346b8d065b5e8827430f3aaef85baf2
GSCrawley <gscrawley@gmail.com>
2020-06-02 13:08:06 -0700

    api key not being defined

---
+++ src/WeekContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import apiConfig from './apiKeys';
+
+class WeekContainer extends React.Component {
+  state = {
+    fullData: [],
+    dailyData: []
+  }
+
+  componentDidMount = () => {
+    const weatherURL =
+    `http://api.openweathermap.org/data/2.5/forecast?zip=94108&units=imperial&APPID=${apiConfig.owmKey}`
+
+    fetch(weatherURL)
+    .then(res => res.json())
+    .then(data => {
+      const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+      this.setState({
+        fullData: data.list,
+        dailyData: dailyData
+      }, () => console.log(this.state))
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+
+export default WeekContainer;
