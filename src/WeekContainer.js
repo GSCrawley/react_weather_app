8018d2d4dcdd5650f51e47f45302651ea812ac01
GSCrawley <gscrawley@gmail.com>
2020-06-04 16:26:38 -0700

    not accessing api

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import apiConfig from './apiKeys';
+import apiConfig from './apiKeys';
 
 class WeekContainer extends React.Component {
   state = {
@@ -9,9 +9,9 @@ class WeekContainer extends React.Component {
 
   componentDidMount = () => {
     const weatherURL =
-    `http://api.openweathermap.org/data/2.5/forecast?zip=94108&units=imperial&APPID=${'32e9b5f03f3da4512a7566f42bf0b37a'}`
+    `http://api.openweathermap.org/data/2.5/forecast?zip=94108&us&APPID=${apiConfig.owmKey}`
 
-    fetch(weatherURL)
+  fetch(weatherURL)
     .then(res => res.json())
     .then(data => {
       const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
